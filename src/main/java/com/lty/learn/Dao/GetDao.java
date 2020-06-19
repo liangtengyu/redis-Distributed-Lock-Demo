bcfb6f2b1a139423e8a5f3597704f08648796543
lty <srevers@vip.qq.com>
2020-06-19 15:41:47 +0800

    Update GetDao.java

--- src/main/java/com/lty/learn/Dao/GetDao.java
+++ src/main/java/com/lty/learn/Dao/GetDao.java
@@ -8,7 +8,7 @@ import java.util.Map;
 @Mapper
 public interface GetDao {
 
-    @Select(" select * from  base_user_car   where username = '13858380692' ")
+    @Select(" select * from  base_user_car   where username = 'xxx' ")
     Map getUser();
 
 
